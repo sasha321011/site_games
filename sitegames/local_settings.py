1ec9af485803ee5628f54341950dd45f4016b07a
sasha321011 <starsagd13@gmail.com>
2024-09-23 19:15:31 +0300

    project

--- sitegames/local_settings.py
+++
@@ -1,9 +0,0 @@
-EMAIL_HOST_USER = 'starsagd13@gmail.com'
-EMAIL_HOST_PASSWORD = 'hwps engq cird axqm'
-
-DB_PASSWORD = 'tubpo'
-
-SECRET_KEY = 'django-insecure-bxi-*mgmzp%&h2n^*tjdjz0dot9)r2p1x3uhlpc==-i5%nuiag'
-# #gmail: hwps engq cird axqm
-# #sitegames
-# #ya mail : mkaxypbkctqkkdgp
